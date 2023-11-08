fbcd3eedb1daa6ff34474570f92e2931168a3840
A Meaningful star <ghrlfehd123@naver.com>
2023-11-08 18:05:37 +0900

    FEAT: 수입기록 페이지 구현
    23.11.08
    
    - 수입기록에 해당하는 데잍터를 노출 시킬 라이브러리 설치
    - 페이지 구현
    
    * 추후 서버에서 데이터 통신 시 기능 구현

--- pages/income.tsx
+++ pages/income.tsx
@@ -1,16 +1,20 @@
 import Layout from "@components/Layout/Layout";
+import Title from "@components/Contents/Title";
+import TableBox from "@components/TableBox";
+import CountBox from "@components/Contents/CountBox";
 
 export default function Income() {
   return (
     <Layout>
       <div className="w-10/12 h-5/6 flex flex-col gap-5">
         <div className="flex flex-row items-end gap-10">
-          <span className="text-3xl font-semibold">수입기록</span>
-          <span className="text-xl font-medium">총 예산: {} 원</span>
+          <Title title="수입기록" />
+          <CountBox title="총 수입" />
         </div>
-        
-        <div className="border border-gray-300 w-full"></div>
-        
+        <div className="border border-gray-300 w-full"/>
+        <div>
+          <TableBox />
+        </div>
       </div>
     </Layout>
   );
