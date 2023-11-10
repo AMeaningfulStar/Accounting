adb2d7b2e633acbe670bdfadb05b4a5f9a54f4bd
A Meaningful star <ghrlfehd123@naver.com>
2023-11-10 15:17:03 +0900

    FEAT: 등록 중 '주차' 등록 컴포넌트
    23.11.10
    
    - '일자' 등록 컴포넌트 구현

---
+++ components/Modal/Registration/WeekInput.tsx
@@ -0,0 +1,38 @@
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+export default function WeekInput({
+  formdata,
+  set,
+}: {
+  formdata: any;
+  set: any;
+}) {
+  return (
+    <div>
+      <FormControl fullWidth size="small">
+        <InputLabel id="demo-select-small-label">주차</InputLabel>
+        <Select
+          color="info"
+          className="bg-white"
+          labelId="demo-select-small-label"
+          id="demo-select-small"
+          value={formdata.week}
+          label="주차"
+          onChange={(e) => set(e.target.value)}
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={"week1"}>1주차</MenuItem>
+          <MenuItem value={"week2"}>2주차</MenuItem>
+          <MenuItem value={"week3"}>3주차</MenuItem>
+          <MenuItem value={"week4"}>4주차</MenuItem>
+          <MenuItem value={"week5"}>5주차</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
