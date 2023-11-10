b9223e84f40b8a0faa416698c1f5b82fdd9c1599
A Meaningful star <ghrlfehd123@naver.com>
2023-11-10 12:44:40 +0900

    STYLE: 레이아웃 수정
    23.11.10
    
    - 레이아웃 수정

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({ children }: LayoutType) {
       <Navbar />
       <div className="w-full h-full p-1.5">
         <div className="w-full h-full flex justify-center items-center bg-white">
-          {children}
+          <div className="w-10/12 h-5/6 flex flex-col gap-5">{children}</div>
         </div>
       </div>
     </div>
