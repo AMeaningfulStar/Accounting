a982a7779837177c19e516915d7dc38b06c3b2d8
A Meaningful star <ghrlfehd123@naver.com>
2023-12-15 14:26:42 +0900

    FIX: content의 header 안에 사용되는 등록 버튼 수정
    23.12.15
    
    - 등록 버튼으로 만들어 둔 것으로 title이라는 인수를 없애고 '등록'이라고 정의

--- components/Contents/Header/RegistrationBtn.tsx
+++ components/Contents/Header/RegistrationBtn.tsx
@@ -1,15 +1,14 @@
 interface RegistrationBtnType{
-  title: string
   openModal: () => void
 }
 
-export default function RegistrationBtn({title, openModal}:RegistrationBtnType) {
+export default function RegistrationBtn({openModal}:RegistrationBtnType) {
   return (
     <button
       className="inline-flex items-center bg-gray-300 border-0 py-2 px-6 focus:outline-none hover:bg-gray-400 rounded text-base mt-4 md:mt-0"
       onClick={openModal}
     >
-      {title}
+      등록
     </button>
   );
 }
