c41cec625b48cff6bc4e0df8bbc806b8036519f4
A Meaningful star <ghrlfehd123@naver.com>
2023-12-15 18:18:49 +0900

    FEAT: API 통신 중 대기 화면 구현
    23.12.15
    
    - 서버와 API 통신 시 공백 시간을 나타낼 로딩창 구현

--- components/Contents/Main/ManagmentTable.tsx
+++ components/Contents/Main/ManagmentTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -15,6 +15,7 @@ import FirstPageIcon from "@mui/icons-material/FirstPage";
 import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 import LastPageIcon from "@mui/icons-material/LastPage";
+import { getManagment } from "@api/axios/managmentAPI";
 
 interface TablePaginationActionsProps {
   count: number;
@@ -106,13 +107,11 @@ interface ManagmentType {
   description: string;
 }
 
-interface ManagmentTableType {
-  tableData: Array<ManagmentType>;
-}
-
-export default function ManagmentTable({ tableData }: ManagmentTableType) {
+export default function ManagmentTable() {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(3);
+  const [tableData, setTableData] = useState<Array<ManagmentType>>([]);
+  const [loading, setLoading] = useState(false);
   const headerNames = [
     "이름",
     "이메일",
@@ -122,6 +121,22 @@ export default function ManagmentTable({ tableData }: ManagmentTableType) {
     "비고",
     "",
   ];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await getManagment();
+        setTableData(response);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
@@ -180,77 +195,85 @@ export default function ManagmentTable({ tableData }: ManagmentTableType) {
   }
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ width: "100%" }} aria-label="custom pagination table">
-        <TableHead className="bg-gray-300">
-          <TableRow>
-            {headerNames &&
-              headerNames.map((name, idx) => (
-                <TableCell
-                  key={idx}
-                  align="center"
-                  className="font-semibold text-sm"
-                >
-                  {name}
-                </TableCell>
-              ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {tableData
-            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            .map((tableData) => (
-              <TableRow key={tableData.id}>
-                <TableCell component="th" scope="row" align="center">
-                  {tableData.name}
-                </TableCell>
-                <TableCell align="center">
-                  {tableData.email}
-                </TableCell>
-                <TableCell align="center">
-                  {formatContactNumber(tableData.contactNumber)}
-                </TableCell>
-                <TableCell align="center">{formatRole(tableData.role)}</TableCell>
-                <TableCell align="center">
-                  {formatRegistrationDate(tableData.registrationDate)}
-                </TableCell>
-                <TableCell align="center">{tableData.description}</TableCell>
-                <TableCell align="center">
-                  <button
-                    className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
+    <div>
+      {loading ? 
+        <div className="w-full flex flex-col justify-center items-center">
+          <span className="text-xl p-10">...loading</span>
+        </div>
+      :  
+        <TableContainer component={Paper}>
+        <Table sx={{ width: "100%" }} aria-label="custom pagination table">
+          <TableHead className="bg-gray-300">
+            <TableRow>
+              {headerNames &&
+                headerNames.map((name, idx) => (
+                  <TableCell
+                    key={idx}
+                    align="center"
+                    className="font-semibold text-sm"
                   >
-                    수정/삭제
-                  </button>
-                </TableCell>
-              </TableRow>
-            ))}
-          {emptyRows > 0 && (
-            <TableRow style={{ height: 65 * emptyRows }}>
-              <TableCell colSpan={8} />
+                    {name}
+                  </TableCell>
+                ))}
             </TableRow>
-          )}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TablePagination
-              rowsPerPageOptions={[3, 5, 7]}
-              colSpan={8}
-              count={tableData.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              SelectProps={{
-                inputProps: {
-                  "aria-label": "rows per page",
-                },
-                native: true,
-              }}
-              onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
-              ActionsComponent={TablePaginationActions}
-            />
-          </TableRow>
-        </TableFooter>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {tableData
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((tableData) => (
+                <TableRow key={tableData.id}>
+                  <TableCell component="th" scope="row" align="center">
+                    {tableData.name}
+                  </TableCell>
+                  <TableCell align="center">
+                    {tableData.email}
+                  </TableCell>
+                  <TableCell align="center">
+                    {formatContactNumber(tableData.contactNumber)}
+                  </TableCell>
+                  <TableCell align="center">{formatRole(tableData.role)}</TableCell>
+                  <TableCell align="center">
+                    {formatRegistrationDate(tableData.registrationDate)}
+                  </TableCell>
+                  <TableCell align="center">{tableData.description}</TableCell>
+                  <TableCell align="center">
+                    <button
+                      className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
+                    >
+                      수정/삭제
+                    </button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 65 * emptyRows }}>
+                <TableCell colSpan={8} />
+              </TableRow>
+            )}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[3, 5, 7]}
+                colSpan={8}
+                count={tableData.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: {
+                    "aria-label": "rows per page",
+                  },
+                  native: true,
+                }}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
+      }
+    </div>
   );
 }
