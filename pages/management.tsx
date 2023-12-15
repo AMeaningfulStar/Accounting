cb6cf0753d246b65a320cd94f4e4604fa58c2928
A Meaningful star <ghrlfehd123@naver.com>
2023-12-15 14:28:14 +0900

    FEAT: managment 페이지의 등록 버튼 구현
    23.12.15
    
    - 등록 버튼 퍼블리싱 구현
    - 등록 버튼 클릭 시 모달창 노출 기능 구현

--- pages/management.tsx
+++ pages/management.tsx
@@ -1,5 +1,7 @@
 import Layout from "@components/Layout/Layout";
 import Title from "@components/Contents/Header/Title";
+import RegistrationBtn from "@components/Contents/Header/RegistrationBtn";
+import ModalLayout from "@components/Modal/ModalLayout";
 
 import ManagmentTable from "@components/Contents/Main/ManagmentTable";
 
@@ -7,6 +9,7 @@ import React, { useEffect, useState } from "react";
 import { getManagment } from "@api/axios/managmentAPI";
 
 export default function Management() {
+  const [ registrationModal, setRegistrationModal ] = useState(false); // 등록 Modal
   const [managmentData, setManagmentData] = useState([]);
 
   useEffect(() => {
@@ -21,15 +24,28 @@ export default function Management() {
 
     fetchData();
   }, []);
+
+  // Modal open
+  const openRegistrationModal = () => {
+    setRegistrationModal(true)
+  }
+
+  // modal close
+  const closeRegistrationModal = () => {
+    setRegistrationModal(false)
+  }
+
   return (
     <Layout>
-      <div className="flex flex-row items-end gap-10">
+      <div className="flex flex-row justify-between">
         <Title title="사용자" />
+        <RegistrationBtn openModal={openRegistrationModal}/>
       </div>
       <div className="border border-gray-300 w-full" />
         <ManagmentTable tableData={managmentData} />
       <div>
       </div>
+      { registrationModal && <ModalLayout title="사용자 등록" closeModal={closeRegistrationModal}>ㄴㅇ</ModalLayout>}
     </Layout>
   );
 }
