3ada1c0cd1600e13b4ee4b68528375f2a74f4d70
A Meaningful star <ghrlfehd123@naver.com>
2023-12-16 15:32:34 +0900

    FEAT: 사용자 등록을 위한 modal 구현
    23.12.16
    
    - 사용자 등록을 위한 Modal 구현
    - 데이터 변동 감지를 위한 dataPlus라는 boolean 변수
    - Modal의 등록 버튼 클릭 시, API 통신 구현

--- pages/management.tsx
+++ pages/management.tsx
@@ -3,49 +3,188 @@ import Title from "@components/Contents/Header/Title";
 import RegistrationBtn from "@components/Contents/Header/RegistrationBtn";
 import ModalLayout from "@components/Modal/ModalLayout";
 
+// 데이터 테이블
 import ManagmentTable from "@components/Contents/Main/ManagmentTable";
 
-import React, { useEffect, useState } from "react";
-import { getManagment } from "@api/axios/managmentAPI";
+// API
+import React, { useState } from "react";
+import { postManagment } from "@api/axios/managmentAPI";
+
+// 모달
+import { TextField } from "@mui/material";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+import ManagementStores from "@stores/ManagmentStore";
 
 export default function Management() {
-  const [ registrationModal, setRegistrationModal ] = useState(false); // 등록 Modal
-  const [managmentData, setManagmentData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await getManagment();
-        setManagmentData(response);
-      } catch (error) {
-        console.error('Error fetching users:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
+  const [registrationModal, setRegistrationModal] = useState(false); // 등록 Modal
+  const [dataPlus, setDataplus] = useState(false); // 데이터 추가 여부
+  const {
+    formdata,
+    setId,
+    setPassword,
+    setEmail,
+    setDepartment,
+    setName,
+    setContactNumber,
+    setRole,
+    setRemarks,
+    clearManagment,
+  } = ManagementStores();
 
   // Modal open
   const openRegistrationModal = () => {
-    setRegistrationModal(true)
-  }
+    setRegistrationModal(true);
+  };
 
   // modal close
   const closeRegistrationModal = () => {
-    setRegistrationModal(false)
-  }
+    setRegistrationModal(false);
+    clearManagment();
+  };
+
+  const handleSubmit = async () => {
+    try {
+      setDataplus(true);
+      const response = await postManagment(formdata);
+
+      if(response){
+        console.log('Success create user')
+      }
+    } catch (error) {
+      console.error("Error create user:", error);
+    } finally {
+      closeRegistrationModal();
+      setDataplus(false);
+    }
+  };
 
   return (
     <Layout>
       <div className="flex flex-row justify-between">
         <Title title="사용자" />
-        <RegistrationBtn openModal={openRegistrationModal}/>
+        <RegistrationBtn openModal={openRegistrationModal} />
       </div>
       <div className="border border-gray-300 w-full" />
-        <ManagmentTable tableData={managmentData} />
-      <div>
-      </div>
-      { registrationModal && <ModalLayout title="사용자 등록" closeModal={closeRegistrationModal}>ㄴㅇ</ModalLayout>}
+      <ManagmentTable dataPlus={dataPlus} />
+      <div></div>
+      {registrationModal && (
+        <ModalLayout title="사용자 등록" closeModal={closeRegistrationModal}>
+          <TextField
+            label="아이디"
+            id="id"
+            size="small"
+            fullWidth
+            type="text"
+            value={formdata.id}
+            style={{ backgroundColor: "white" }}
+            onChange={(e) => setId(e.target.value)}
+            color="info"
+          />
+          <TextField
+            label="비밀번호"
+            id="password"
+            size="small"
+            fullWidth
+            type="password"
+            value={formdata.password}
+            style={{ backgroundColor: "white" }}
+            onChange={(e) => setPassword(e.target.value)}
+            color="info"
+          />
+          <TextField
+            label="이메일"
+            id="email"
+            size="small"
+            fullWidth
+            type="email"
+            value={formdata.email}
+            style={{ backgroundColor: "white" }}
+            onChange={(e) => setEmail(e.target.value)}
+            color="info"
+          />
+          <TextField
+            label="이름"
+            id="name"
+            size="small"
+            fullWidth
+            type="text"
+            value={formdata.name}
+            style={{ backgroundColor: "white" }}
+            onChange={(e) => setName(e.target.value)}
+            color="info"
+          />
+          <TextField
+            label="전화번호 (예시: 01012345678)"
+            id="contactNumber"
+            size="small"
+            fullWidth
+            type="text"
+            value={formdata.contactNumber}
+            style={{ backgroundColor: "white" }}
+            onChange={(e) => setContactNumber(e.target.value)}
+            color="info"
+          />
+          <FormControl fullWidth size="small">
+            <InputLabel id="demo-select-small-label">부서</InputLabel>
+            <Select
+              color="info"
+              className="bg-white"
+              labelId="demo-select-small-label"
+              id="department"
+              value={formdata.department}
+              label="부서"
+              onChange={(e) => setDepartment(e.target.value)}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={"청소년1부"}>청소년1부</MenuItem>
+              <MenuItem value={"청소년1부+"}>청소년1부+</MenuItem>
+              <MenuItem value={"청소년2부"}>청소년2부</MenuItem>
+              <MenuItem value={"청소년3부"}>청소년3부</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl fullWidth size="small">
+            <InputLabel id="demo-select-small-label">권한</InputLabel>
+            <Select
+              color="info"
+              className="bg-white"
+              labelId="demo-select-small-label"
+              id="role"
+              value={formdata.role}
+              label="권한"
+              onChange={(e) => setRole(e.target.value)}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={"user"}>일반회원</MenuItem>
+              <MenuItem value={"admin"}>관리자</MenuItem>
+            </Select>
+          </FormControl>
+          <TextField
+            label="비고"
+            id="remarks"
+            size="small"
+            fullWidth
+            type="text"
+            value={formdata.remarks}
+            style={{ backgroundColor: "white" }}
+            onChange={(e) => setRemarks(e.target.value)}
+            color="info"
+          />
+          <button
+            className="text-white bg-[#7D8DA7] active:bg-[#8FA3C3] border-0 py-2 px-8 mt-2 focus:outline-none rounded text-lg"
+            onClick={() => handleSubmit()}
+          >
+            등록
+          </button>
+        </ModalLayout>
+      )}
     </Layout>
   );
 }
